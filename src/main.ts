3c73806b0e88c048d04d30da24c612294a3e149d
Sion Kim <94289633+mandlake@users.noreply.github.com>
2025-05-21 14:11:02 +0900

    fix : style.css 파일 위치 수정

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 
-import "./assets/main.css";
+import "./assets/style.css";
 
 const app = createApp(App);
 app.use(createPinia());
